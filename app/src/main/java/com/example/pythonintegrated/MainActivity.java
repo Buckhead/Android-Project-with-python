0341a70358ca32f2705bfb601d43709f4282b0f1
Buckhead <buckhead.korea@gamil.com>
2020-09-13 09:09:07 +0900

    Calling python codes from Android studio

---
+++ app/src/main/java/com/example/pythonintegrated/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.pythonintegrated;
+
+import android.os.Bundle;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Et1, Et2;
+    Button Btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Et1 = (EditText)findViewById(R.id.et1);
+        Et2 = (EditText)findViewById(R.id.et2);
+        Btn = (Button)findViewById(R.id.btn);
+        tv = (TextView)findViewById(R.id.text_view);
+
+        if(!Python.isStarted())
+            Python.start(new AndroidPlatform(this));
+        Python py = Python.getInstance();
+        final PyObject pyobj = py.getModule("script"); // Here we will give name of our python file
+
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                                     //(function name, first argument, second argument).  Call a function within the module('script')
+                PyObject obj = pyobj.callAttr("main", Et1.getText().toString(), Et2.getText().toString());
+                // now obj will contain our resut, so set its result to textview
+                tv.setText(obj.toString());
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/python/script.py
@@ -0,0 +1,8 @@
+def main(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    sum= num1+num2
+    return "sum is "+str(sum)
+# so our script is ready, this script main function will receive number1 and 2 as string 
+# then we convert it into int then we add these number and finally return with sum(in string)
+
